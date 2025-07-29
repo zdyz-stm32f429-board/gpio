97a0c894473f0879eb1fffae5f72500a8f336ea5
zhengjy <1173131202@qq.com>
2025-07-29 18:05:05 +0800

    按键驱动框架修改正在进行中

--- user/main.c
+++ user/main.c
@@ -30,25 +30,7 @@ size_t pin_id = 0;
 /* Private function prototypes -----------------------------------------------*/
 
 /* Private functions ---------------------------------------------------------*/
-static void key0_handle(void *args)
-{
-    printf("key0_down!\r\n");
-}
 
-static void key1_handle(void *args)
-{
-    printf("key1_down!\r\n");
-}
-
-static void key2_handle(void *args)
-{
-    printf("key2_down!\r\n");
-}
-
-static void wkup_key_handle(void *args)
-{
-    printf("wkup_key_down!\r\n");
-}
 /* Exported functions --------------------------------------------------------*/
 /**
  * @brief  Main program
@@ -70,19 +52,6 @@ int main(void)
     gpio_set_mode(LED_BLUE_PIN_ID, PIN_OUTPUT_PP, PIN_PULL_UP);
     gpio_write(LED_RED_PIN_ID, 1);
     gpio_write(LED_BLUE_PIN_ID, 1);
-    
-    /* set key gpio mode */
-    gpio_attach_irq(KEY0_PIN_ID, PIN_EVENT_FALLING_EDGE, key0_handle, NULL);
-    gpio_irq_enable(KEY0_PIN_ID, 1);
-    
-    gpio_attach_irq(KEY1_PIN_ID, PIN_EVENT_FALLING_EDGE, key1_handle, NULL);
-    gpio_irq_enable(KEY1_PIN_ID, 1);
-    
-    gpio_attach_irq(KEY2_PIN_ID, PIN_EVENT_FALLING_EDGE, key2_handle, NULL);
-    gpio_irq_enable(KEY2_PIN_ID, 1);
-    
-    gpio_attach_irq(WKUP_PIN_ID, PIN_EVENT_RISING_EDGE, wkup_key_handle, NULL);
-    gpio_irq_enable(WKUP_PIN_ID, 1);
     
     while (1)
     {
